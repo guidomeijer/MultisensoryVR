20e9aa51936f558a12af19ecf751b37ffa7ea98d
Guido <guido.meijer@donders.ru.nl>
2023-08-28 15:12:08 +0200

    .

--- Behavior/plot_speed_objects_training.py
+++ Behavior/plot_speed_objects_training.py
@@ -14,6 +14,8 @@ from os.path import join, isfile
 import pandas as pd
 from msvr_functions import load_subjects, paths
 
+SPEED_BIN_SIZE = 0.1  # s
+
 # Get subjects
 subjects = load_subjects()
 
@@ -31,8 +33,20 @@ for i, subject in enumerate(subjects['SubjectID']):
             # Load in data
             trials = pd.read_csv(join(path_dict['server_path'], 'Subjects', subject, ses, 'trials.csv'),
                                  index_col=0)
-            wheel_distance = np.load(join(path_dict['server_path'], 'Subjects', subject, ses, 'wheel.distance.npy'))
+            wheel_distance = np.load(join(path_dict['server_path'], 'Subjects', subject, ses, 'wheel.distance.npy')).astype(float)
             wheel_times = np.load(join(path_dict['server_path'], 'Subjects', subject, ses, 'wheel.times.npy'))
             
-        # Calculate speed
-        binned_statistic(wheel_distance)
+            # Calculate speed
+            speed_time_bins = np.arange(wheel_times[0], wheel_times[-1], SPEED_BIN_SIZE)
+            speed_time = speed_time_bins[:-1] + SPEED_BIN_SIZE/2
+            speed = np.empty(speed_time.shape)
+            for ii, tb in enumerate(speed_time_bins[:-1]):
+                this_dist = np.sum(np.abs(np.diff(
+                    wheel_distance[(wheel_times >= tb) & (wheel_times < speed_time_bins[ii+1])])))
+                speed[ii] = this_dist / SPEED_BIN_SIZE
+                if speed[ii] > 100:
+                    sdf
+                
+            
+            asd
+            binned_statistic(wheel_distance)
