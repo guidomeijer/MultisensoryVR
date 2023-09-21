c39cee08ff66ac4a9e12a4e2da63acd6c717af4f
Guido <guido.meijer@donders.ru.nl>
2023-09-21 10:14:30 +0200

    .

--- Behavior/plot_breathing_example.py
+++ Behavior/plot_breathing_example.py
@@ -7,7 +7,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from os.path import join
+from scipy.signal import butter, filtfilt
 from msvr_functions import paths, figure_style
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter(order, [lowcut, highcut], fs=fs, btype='band')
+    y = filtfilt(b, a, data)
+    return y
 
 
 # Get paths
@@ -15,13 +22,20 @@ path_dict = paths()
 data_path = path_dict['local_data_path']
 
 # Load in data
-breathing = np.load(join(data_path, '450508', '20230920', 'continuous.breathing.npy'))
-timestamps = np.load(join(data_path, '450508', '20230920', 'continuous.times.npy'))
+breathing = np.load(join(data_path, 'Subjects', '450409', '20230920', 'continuous.breathing.npy'))
+timestamps = np.load(join(data_path, 'Subjects', '450409', '20230920', 'continuous.times.npy'))
+
+# Filter trace
+breathing = breathing - np.mean(breathing)
+breathing_filt = butter_bandpass_filter(breathing, 1, 12, int(1/np.diff(timestamps)[0]), order=1)
 
 # Plot
 colors, dpi = figure_style()
 f, ax1 = plt.subplots(figsize=(1.75, 2), dpi=dpi)
 ax1.plot(timestamps[:1000], breathing[:1000])
+ax1.plot(timestamps[:1000], breathing_filt[:1000])
+ax1.set(yticks=[], xlabel='Time (s)')
 
 plt.tight_layout()
 sns.despine(trim=True, left=True)
+plt.savefig(join(path_dict['fig_path'], 'breathing_example.jpg'), dpi=600)
