8212f4794c325e7d1811381cb3e31544a2f26f3f
Guido <guido.meijer@donders.ru.nl>
2024-04-04 12:23:03 +0200

    New extraction

--- Pipeline/behavioral_extraction.py
+++ Pipeline/behavioral_extraction.py
@@ -115,20 +115,28 @@ for root, directory, files in chain.from_iterable(os.walk(path) for path in sear
 
         # Extract all event timings
         all_env_end = time_s[compute_onsets(data['digitalIn'][:, 12])]
-        all_first_obj_appear = time_s[compute_onsets(data['digitalIn'][:, 13])]
-        all_reward_zones = time_s[compute_onsets(data['digitalIn'][:, 14])]
-        all_obj1_enter = time_s[compute_onsets(data['digitalIn'][:, 8])]
-        all_obj2_enter = time_s[compute_onsets(data['digitalIn'][:, 9])]
-        all_obj3_enter = time_s[compute_onsets(data['digitalIn'][:, 10])]
-        all_obj1_exit = time_s[compute_offsets(data['digitalIn'][:, 8])]
-        all_obj2_exit = time_s[compute_offsets(data['digitalIn'][:, 9])]
-        all_obj3_exit = time_s[compute_offsets(data['digitalIn'][:, 10])]
-        all_sound0_onsets = time_s[compute_onsets(data['digitalIn'][:, 5])]
-        all_sound1_onsets = time_s[compute_onsets(data['digitalIn'][:, 6])]
-        all_sound2_onsets = time_s[compute_onsets(data['digitalIn'][:, 7])]
-        all_sound0_offsets = time_s[compute_onsets(data['digitalIn'][:, 5])]
-        all_sound1_offsets = time_s[compute_onsets(data['digitalIn'][:, 6])]
-        all_sound2_offsets = time_s[compute_onsets(data['digitalIn'][:, 7])]
+        all_surprise = time_s[compute_onsets(data['digitalIn'][:, 1])]
+        
+        all_obj1_enter = time_s[compute_onsets(data['digitalIn'][:, 4])]
+        all_obj2_enter = time_s[compute_onsets(data['digitalIn'][:, 5])]
+        all_obj3_enter = time_s[compute_onsets(data['digitalIn'][:, 6])]
+        all_obj4_enter = time_s[compute_onsets(data['digitalIn'][:, 7])]
+        all_obj5_enter = time_s[compute_onsets(data['digitalIn'][:, 8])]
+        
+        all_obj1_exit = time_s[compute_offsets(data['digitalIn'][:, 4])]
+        all_obj2_exit = time_s[compute_offsets(data['digitalIn'][:, 5])]
+        all_obj3_exit = time_s[compute_offsets(data['digitalIn'][:, 6])]
+        all_obj4_exit = time_s[compute_offsets(data['digitalIn'][:, 7])]
+        all_obj5_exit = time_s[compute_offsets(data['digitalIn'][:, 8])]
+        
+        all_sound1_onsets = time_s[compute_onsets(data['digitalIn'][:, 9])]
+        all_sound2_onsets = time_s[compute_onsets(data['digitalIn'][:, 10])]
+        all_sound1_offsets = time_s[compute_onsets(data['digitalIn'][:, 9])]
+        all_sound2_offsets = time_s[compute_onsets(data['digitalIn'][:, 10])]
+        
+        all_obj1_appear = time_s[compute_onsets(data['digitalIn'][:, 13])]
+        all_obj2_appear = time_s[compute_onsets(data['digitalIn'][:, 14])]
+        all_obj3_appear = time_s[compute_onsets(data['digitalIn'][:, 15])]
         
         # Only keep environment entries which have a first object appear event and a trial end event
         # TO DO: change to object appear
@@ -144,22 +152,32 @@ for root, directory, files in chain.from_iterable(os.walk(path) for path in sear
 
         # Pre-allocate trial arrays
         env_end = np.empty(env_start.shape[0]-1)
-        first_obj_appear = np.empty(env_start.shape[0]-1)
-        obj1_enter = np.empty(env_start.shape[0]-1)
-        obj2_enter = np.empty(env_start.shape[0]-1)
-        obj3_enter = np.empty(env_start.shape[0]-1)
-        obj1_exit = np.empty(env_start.shape[0]-1)
-        obj2_exit = np.empty(env_start.shape[0]-1)
-        obj3_exit = np.empty(env_start.shape[0]-1)
-        obj1_rewardzone = np.empty(env_start.shape[0]-1)
-        obj2_rewardzone = np.empty(env_start.shape[0]-1)
-        obj3_rewardzone = np.empty(env_start.shape[0]-1)
+        surprise_time = np.zeros(env_start.shape[0]-1).astype(bool)
+        
+        obj1_appear = np.zeros(env_start.shape[0]-1).astype(bool)
+        obj2_appear = np.zeros(env_start.shape[0]-1).astype(bool)
+        obj3_appear = np.zeros(env_start.shape[0]-1).astype(bool)
+        
+        obj1_enter = np.zeros(env_start.shape[0]-1).astype(bool)
+        obj2_enter = np.zeros(env_start.shape[0]-1).astype(bool)
+        obj3_enter = np.zeros(env_start.shape[0]-1).astype(bool)
+        obj4_enter = np.zeros(env_start.shape[0]-1).astype(bool)
+        obj5_enter = np.zeros(env_start.shape[0]-1).astype(bool)
+        
+        obj1_exit = np.zeros(env_start.shape[0]-1).astype(bool)
+        obj2_exit = np.zeros(env_start.shape[0]-1).astype(bool)
+        obj3_exit = np.zeros(env_start.shape[0]-1).astype(bool)
+        obj4_exit = np.zeros(env_start.shape[0]-1).astype(bool)
+        obj5_exit = np.zeros(env_start.shape[0]-1).astype(bool)
+        
         obj1_rewards = np.zeros(env_start.shape[0]-1).astype(int)
         obj2_rewards = np.zeros(env_start.shape[0]-1).astype(int)
         obj3_rewards = np.zeros(env_start.shape[0]-1).astype(int)
+        
         obj1_position = np.zeros(env_start.shape[0]-1).astype(int)
         obj2_position = np.zeros(env_start.shape[0]-1).astype(int)
         obj3_position = np.zeros(env_start.shape[0]-1).astype(int)
+        
         sound_onset = np.empty(env_start.shape[0]-1)
         sound_offset = np.empty(env_start.shape[0]-1)
         sound_id = np.zeros(env_start.shape[0]-1).astype(int)
@@ -167,61 +185,54 @@ for root, directory, files in chain.from_iterable(os.walk(path) for path in sear
         # Loop over trials and get events per trial
         for i, ts in enumerate(env_start[:-1]):
 
+            # Surprise event
+            these_surprise = all_surprise[(all_surprise > ts)
+                                          & (all_surprise < env_start[i+1])]
+            if len(these_surprise) > 0:
+                surprise_time[i] = these_surprise[0]
+                        
             # Object enter and exit events
-            these_obj1_enter = all_obj1_enter[(all_obj1_enter > ts) & (
-                all_obj1_enter < env_start[i+1])]
+            these_obj1_enter = all_obj1_enter[(all_obj1_enter > ts)
+                                              & (all_obj1_enter < env_start[i+1])]
             if len(these_obj1_enter) > 0:
                 obj1_enter[i] = these_obj1_enter[0]
-            else:
-                obj1_enter[i] = np.nan
-            these_obj2_enter = all_obj2_enter[(all_obj2_enter > ts) & (
-                all_obj2_enter < env_start[i+1])]
+            these_obj2_enter = all_obj2_enter[(all_obj2_enter > ts)
+                                              & (all_obj2_enter < env_start[i+1])]
             if len(these_obj2_enter) > 0:
                 obj2_enter[i] = these_obj2_enter[0]
-            else:
-                obj2_enter[i] = np.nan
-            these_obj3_enter = all_obj3_enter[(all_obj3_enter > ts) & (
-                all_obj3_enter < env_start[i+1])]
+            these_obj3_enter = all_obj3_enter[(all_obj3_enter > ts)
+                                              & (all_obj3_enter < env_start[i+1])]
             if len(these_obj3_enter) > 0:
                 obj3_enter[i] = these_obj3_enter[0]
-            else:
-                obj3_enter[i] = np.nan
-            these_obj1_exit = all_obj1_exit[(all_obj1_exit > ts) &
-                                            (all_obj1_exit < env_start[i+1])]
+            these_obj4_enter = all_obj4_enter[(all_obj4_enter > ts)
+                                              & (all_obj4_enter < env_start[i+1])]
+            if len(these_obj4_enter) > 0:
+                obj4_enter[i] = these_obj4_enter[0]
+            these_obj5_enter = all_obj3_enter[(all_obj5_enter > ts)
+                                              & (all_obj3_enter < env_start[i+1])]
+            if len(these_obj5_enter) > 0:
+                obj5_enter[i] = these_obj5_enter[0]
+            
+            these_obj1_exit = all_obj1_exit[(all_obj1_exit > ts) 
+                                            & (all_obj1_exit < env_start[i+1])]
             if len(these_obj1_exit) > 0:
                 obj1_exit[i] = these_obj1_exit[0]
-            else:
-                obj1_exit[i] = np.nan
-            these_obj2_exit = all_obj2_exit[(all_obj2_exit > ts) &
-                                            (all_obj2_exit < env_start[i+1])]
+            these_obj2_exit = all_obj2_exit[(all_obj2_exit > ts) 
+                                            & (all_obj2_exit < env_start[i+1])]
             if len(these_obj2_exit) > 0:
                 obj2_exit[i] = these_obj2_exit[0]
-            else:
-                obj2_exit[i] = np.nan
-            these_obj3_exit = all_obj3_exit[(all_obj3_exit > ts) &
-                                            (all_obj3_exit < env_start[i+1])]
+            these_obj3_exit = all_obj3_exit[(all_obj3_exit > ts)
+                                            & (all_obj3_exit < env_start[i+1])]
             if len(these_obj3_exit) > 0:
-                obj3_exit[i] = these_obj3_exit[0]
-            else:
-                obj3_exit[i] = np.nan
-            these_obj1_rz = all_reward_zones[(all_reward_zones > obj1_enter[i]) &
-                                             (all_reward_zones < obj1_exit[i])]
-            if len(these_obj1_rz) > 0:
-                obj1_rewardzone[i] = these_obj1_rz[0]
-            else:
-                obj1_rewardzone[i] = np.nan
-            these_obj2_rz = all_reward_zones[(all_reward_zones > obj2_enter[i]) &
-                                             (all_reward_zones < obj2_exit[i])]
-            if len(these_obj2_rz) > 0:
-                obj2_rewardzone[i] = these_obj2_rz[0]
-            else:
-                obj2_rewardzone[i] = np.nan
-            these_obj3_rz = all_reward_zones[(all_reward_zones > obj3_enter[i]) &
-                                             (all_reward_zones < obj3_exit[i])]
-            if len(these_obj3_rz) > 0:
-                obj3_rewardzone[i] = these_obj3_rz[0]
-            else:
-                obj3_rewardzone[i] = np.nan
+                obj3_exit[i] = these_obj3_exit[0]   
+            these_obj4_exit = all_obj4_exit[(all_obj4_exit > ts)
+                                            & (all_obj4_exit < env_start[i+1])]
+            if len(these_obj4_exit) > 0:
+                obj4_exit[i] = these_obj4_exit[0]   
+            these_obj5_exit = all_obj5_exit[(all_obj5_exit > ts)
+                                            & (all_obj5_exit < env_start[i+1])]
+            if len(these_obj5_exit) > 0:
+                obj5_exit[i] = these_obj5_exit[0]   
 
             # Number of rewards given per object
             obj1_rewards[i] = np.sum((reward_times >= obj1_enter[i]-0.1)
@@ -237,21 +248,21 @@ for root, directory, files in chain.from_iterable(os.walk(path) for path in sear
             obj3_position[i] = np.argsort([obj1_enter[i], obj2_enter[i], obj3_enter[i]])[2] + 1
 
             # Timestamp of appearance of object in first position
-            these_first_obj = all_first_obj_appear[(all_first_obj_appear > ts) & (
-                all_first_obj_appear < env_start[i+1])]
-            if len(these_first_obj) > 0:
-                first_obj_appear[i] = these_first_obj[0]
-            else:
-                first_obj_appear[i] = np.nan
+            these_obj1_appear = all_obj1_appear[(all_obj1_appear > ts)
+                                                & (all_obj1_appear < env_start[i+1])]
+            if len(these_obj1_appear) > 0:
+                obj1_appear[i] = these_obj1_appear[0]
+            these_obj2_appear = all_obj1_appear[(all_obj1_appear > ts)
+                                                & (all_obj1_appear < env_start[i+1])]
+            if len(these_obj2_appear) > 0:
+                obj2_appear[i] = these_obj2_appear[0]
+            these_obj3_appear = all_obj3_appear[(all_obj3_appear > ts)
+                                                & (all_obj3_appear < env_start[i+1])]
+            if len(these_obj3_appear) > 0:
+                obj3_appear[i] = these_obj3_appear[0]
 
             # Sound on and offsets
-            if all_sound0_onsets[(all_sound0_onsets > ts) & (all_sound0_onsets < env_start[i+1])].shape[0] > 0:
-                sound_id[i] = 0
-                sound_onset[i] = all_sound0_onsets[(all_sound0_onsets > ts) & (
-                    all_sound0_onsets < env_start[i+1])][0]
-                sound_offset[i] = all_sound0_offsets[(all_sound0_offsets > ts) & (
-                    all_sound0_offsets < env_start[i+1])][0]
-            elif all_sound1_onsets[(all_sound1_onsets > ts) & (all_sound1_onsets < env_start[i+1])].shape[0] > 0:
+            if all_sound1_onsets[(all_sound1_onsets > ts) & (all_sound1_onsets < env_start[i+1])].shape[0] > 0:
                 sound_id[i] = 1
                 sound_onset[i] = all_sound1_onsets[(all_sound1_onsets > ts) & (
                     all_sound1_onsets < env_start[i+1])][0]
@@ -312,12 +323,10 @@ for root, directory, files in chain.from_iterable(os.walk(path) for path in sear
             'enterEnvTime': env_start[:-1], 'exitEnvTime': env_end,
             'enterEnvPos': env_start_pos, 'exitEnvPos': env_end_pos,
             'soundOnset': sound_onset, 'soundOffset': sound_offset, 'soundId': sound_id,
-            'firstObjectAppear': first_obj_appear,
+            'appearObj1': obj1_appear,
             'enterObj1': obj1_enter, 'enterObj2': obj2_enter, 'enterObj3': obj3_enter,
             'enterObj1Pos': obj1_enter_pos, 'enterObj2Pos': obj2_enter_pos, 'enterObj3Pos': obj3_enter_pos,
             'exitObj1': obj1_exit, 'exitObj2': obj2_exit, 'exitObj3': obj3_exit,
-            'enterRewardZoneObj1': obj1_rewardzone, 'enterRewardZoneObj2': obj2_rewardzone,
-            'enterRewardZoneObj3': obj3_rewardzone,
             'rewardsObj1': obj1_rewards, 'rewardsObj2': obj2_rewards, 'rewardsObj3': obj3_rewards,
             'positionObj1': obj1_position, 'positionObj2': obj2_position, 'positionObj3': obj3_position
         })
