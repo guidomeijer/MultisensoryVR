3825a92f35d26031f1aadbe9eb0ae0ef8b9300db
Guido Meijer <guido.meijer@research.fchampalimaud.org>
2024-08-08 14:18:30 +0200

    correlation with significant neurons

--- Ephys/plot_interarea_correlation.py
+++ Ephys/plot_interarea_correlation.py
@@ -13,15 +13,35 @@ colors, dpi = figure_style()
 
 # Load in data
 path_dict = paths()
-corr_df = pd.read_csv(join(path_dict['save_path'], 'region_corr_25ms-bins.csv'))
+corr_df = pd.read_csv(join(path_dict['save_path'], 'region_corr_100ms-bins.csv'))
 
 # Plot
 region_pairs = np.unique(corr_df['region_pair'])
-f, axs = plt.subplots(1, len(region_pairs), figsize=(1.75*len(region_pairs), 1.75), dpi=dpi)
+f, axs = plt.subplots(1, len(region_pairs), figsize=(1.75*len(region_pairs), 1.75), dpi=dpi,
+                      sharey=True)
 for i, region_pair in enumerate(region_pairs):
+    
+    """
     long_df = pd.melt(corr_df[corr_df['region_pair'] == region_pair],
                       id_vars='time', value_vars=['r_goal_baseline', 'r_distractor_baseline'])
     sns.lineplot(data=long_df, x='time', y='value', hue='variable', ax=axs[i], errorbar='se',
                  legend=None, hue_order=['r_goal_baseline', 'r_distractor_baseline'],
                  palette=[colors['goal'], colors['no-goal']])
+    """
+    this_df = corr_df[corr_df['region_pair'] == region_pair]
+    axs[i].plot(this_df['time'], this_df['r_goal_baseline'], color=colors['goal'])
+    axs[i].fill_between(this_df['time'],
+                        this_df['r_goal_baseline'] - this_df['r_sem_goal'],
+                        this_df['r_goal_baseline'] + this_df['r_sem_goal'],
+                        color=colors['goal'], alpha=0.25, lw=0)
+    axs[i].plot(this_df['time'], this_df['r_distractor_baseline'], color=colors['no-goal'])
+    axs[i].fill_between(this_df['time'],
+                        this_df['r_distractor_baseline'] - this_df['r_sem_distractor'],
+                        this_df['r_distractor_baseline'] + this_df['r_sem_distractor'],
+                        color=colors['no-goal'], alpha=0.25, lw=0)
+    
     axs[i].set(title=f'{region_pair}')
+    
+axs[0].set(ylabel='Correlation (r)', yticks=[-0.04, 0, 0.04])
+sns.despine(trim=True)
+plt.tight_layout()
--- Ephys/run_interarea_correlation.py
+++ Ephys/run_interarea_correlation.py
@@ -7,8 +7,8 @@ import numpy as np
 from os.path import join
 import pandas as pd
 from joblib import Parallel, delayed
-from scipy.stats import pearsonr
-from msvr_functions import paths, load_neural_data, load_subjects, calculate_peths
+from scipy.stats import pearsonr, sem
+from msvr_functions import paths, load_neural_data, load_subjects, calculate_peths, load_objects
 
 # Settings
 SUBJECT = '459601'
@@ -16,9 +16,10 @@ DATE = '20240411'
 PROBE = 'probe00'
 T_BEFORE = 1
 T_AFTER = 2
-BIN_SIZE = 0.025
-SMOOTHING = 0.05
-SUBTRACT_MEAN = False
+BIN_SIZE = 0.1
+SMOOTHING = 0.15
+SUBTRACT_MEAN = True
+MIN_NEURONS = 10  # per region
 
 # Initialize
 path_dict = paths()
@@ -28,28 +29,9 @@ subjects = load_subjects()
 session_path = join(path_dict['local_data_path'], 'Subjects', f'{SUBJECT}', f'{DATE}')
 spikes, clusters, channels = load_neural_data(session_path, PROBE)
 trials = pd.read_csv(join(path_dict['local_data_path'], 'Subjects', SUBJECT, DATE, 'trials.csv'))
-
-# Get reward contingencies
-sound1_obj = subjects.loc[subjects['SubjectID'] == SUBJECT, 'Sound1Obj'].values[0]
-sound2_obj = subjects.loc[subjects['SubjectID'] == SUBJECT, 'Sound2Obj'].values[0]
-control_obj = subjects.loc[subjects['SubjectID'] == SUBJECT, 'ControlObject'].values[0]
-
-obj1_goal_sound = np.where(np.array([sound1_obj, sound2_obj, control_obj]) == 1)[0][0] + 1
-obj2_goal_sound = np.where(np.array([sound1_obj, sound2_obj, control_obj]) == 2)[0][0] + 1
-obj3_goal_sound = np.where(np.array([sound1_obj, sound2_obj, control_obj]) == 3)[0][0] + 1
-
-# Prepare trial data
-rew_obj1_df = pd.DataFrame(data={'times': trials[f'enterObj{sound1_obj}'],
-                                 'object': 1, 'sound': trials['soundId'],
-                                 'goal': (trials['soundId'] == obj1_goal_sound).astype(int)})
-rew_obj2_df = pd.DataFrame(data={'times': trials[f'enterObj{sound2_obj}'],
-                                 'object': 2, 'sound': trials['soundId'],
-                                 'goal': (trials['soundId'] == obj2_goal_sound).astype(int)})
-control_obj_df = pd.DataFrame(data={'times': trials[f'enterObj{control_obj}'],
-                                    'object': 3, 'sound': trials['soundId'],
-                                    'goal': (trials['soundId'] == obj3_goal_sound).astype(int)})
-all_obj_df = pd.concat((rew_obj1_df, rew_obj2_df, control_obj_df))
-all_obj_df = all_obj_df.sort_values(by='times').reset_index(drop=True)
+neurons_df = pd.read_csv(join(path_dict['save_path'], 'significant_neurons.csv'))
+all_obj_df = load_objects(SUBJECT, DATE)
+rew_obj2_df = all_obj_df[all_obj_df['object'] == 2]
 
 # %% Fuction for parallization
 
@@ -60,7 +42,8 @@ def run_correlation(goal_counts, distractor_counts, tt):
             r, _ = pearsonr(goal_counts[region_1][:, n1, tt],
                             goal_counts[region_2][:, n2, tt])
             pairwise_corr.append(r)
-    corr_goal = np.nanmean(pairwise_corr)
+    mean_goal = np.nanmean(pairwise_corr)
+    sem_goal = sem(pairwise_corr, nan_policy='omit')
     
     pairwise_corr = []
     for n1 in range(distractor_counts[region_1].shape[1]):  # Neurons in region 1
@@ -68,9 +51,10 @@ def run_correlation(goal_counts, distractor_counts, tt):
             r, _ = pearsonr(distractor_counts[region_1][:, n1, tt],
                             distractor_counts[region_2][:, n2, tt])
             pairwise_corr.append(r)
-    corr_dis = np.nanmean(pairwise_corr)
+    mean_dis = np.nanmean(pairwise_corr)
+    sem_dis = sem(pairwise_corr, nan_policy='omit')
 
-    return corr_goal, corr_dis
+    return mean_goal, sem_goal, mean_dis, sem_dis
 
 
 # %%
@@ -80,17 +64,24 @@ corr_df = pd.DataFrame()
 for j, region in enumerate(np.unique(clusters['region'])):
     if region == 'root':
         continue
+    
+    # Select neurons
+    region_neurons = clusters['cluster_id'][clusters['region'] == region]
+    #sig_neurons = neurons_df.loc[neurons_df['sig_goal'] | neurons_df['sig_obj_onset'], 'neuron_id'].values
+    sig_neurons = neurons_df.loc[neurons_df['sig_obj_onset'], 'neuron_id'].values
+    #sig_neurons = neurons_df.loc[neurons_df['sig_goal'], 'neuron_id'].values
+    use_neurons = region_neurons[np.isin(region_neurons, sig_neurons)]
+    if use_neurons.shape[0] < MIN_NEURONS:
+        continue
 
     # Get spike counts
     peths, goal_counts[region] = calculate_peths(
-        spikes['times'], spikes['clusters'],
-        clusters['cluster_id'][clusters['region'] == region],
+        spikes['times'], spikes['clusters'], use_neurons,
         rew_obj2_df.loc[rew_obj2_df['goal'] == 1, 'times'], T_BEFORE, T_AFTER, BIN_SIZE, SMOOTHING,
         return_fr=False)
     
     peths, distractor_counts[region] = calculate_peths(
-        spikes['times'], spikes['clusters'],
-        clusters['cluster_id'][clusters['region'] == region],
+        spikes['times'], spikes['clusters'], use_neurons,
         rew_obj2_df.loc[rew_obj2_df['goal'] == 0, 'times'], T_BEFORE, T_AFTER, BIN_SIZE, SMOOTHING,
         return_fr=False)
 
@@ -107,7 +98,9 @@ for r1, region_1 in enumerate(these_regions[:-1]):
             delayed(run_correlation)(goal_counts, distractor_counts, tt)
             for tt in range(tscale.shape[0]))
         corr_goal = np.array([result[0] for result in results])
-        corr_dis = np.array([result[1] for result in results])
+        sem_goal = np.array([result[1] for result in results])
+        corr_dis = np.array([result[2] for result in results])
+        sem_dis = np.array([result[3] for result in results])
         
         # Baseline subtract        
         corr_goal_bl = corr_goal - np.mean(corr_goal[tscale < 0])
@@ -116,10 +109,11 @@ for r1, region_1 in enumerate(these_regions[:-1]):
         # Add to dataframe
         corr_df = pd.concat((corr_df, pd.DataFrame(data={
             'r_goal': corr_goal, 'r_goal_baseline': corr_goal_bl,
+            'r_sem_goal': sem_goal, 'r_sem_distractor': sem_dis,
             'r_distractor': corr_dis, 'r_distractor_baseline': corr_dis_bl,
             'time': tscale, 'region_1': region_1, 'region_2': region_2,
             'region_pair': f'{region_1}-{region_2}',
             'subject': SUBJECT, 'date': DATE})), ignore_index=True)
-
+        
 # Save to disk
 corr_df.to_csv(join(path_dict['save_path'], f'region_corr_{int(BIN_SIZE*1000)}ms-bins.csv'))
